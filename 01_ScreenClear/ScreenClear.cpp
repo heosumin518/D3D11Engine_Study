9befa4fc415675a2143a05141f31ecae750cec93
heosumin <heosumin518@gmail.com>
2023-09-10 00:53:19 +0900

    필요없는 코드 삭제

--- 01_ScreenClear/ScreenClear.cpp
+++ 01_ScreenClear/ScreenClear.cpp
@@ -24,26 +24,6 @@ void ScreenClear::Render()
 	RenderBegin();
 
 	// TO DO
-		// IA - VS - RS - PS - OM
-	{
-		uint32 stride = sizeof(Vertex);
-		uint32 offset = 0;
-		// IA
-		m_deviceContext->IASetVertexBuffers(0, 1, m_vertexBuffer.GetAddressOf(), &stride, &offset);
-		m_deviceContext->IASetInputLayout(m_inputLayout.Get());
-		m_deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);		// 사각형은 strip으로
-
-		// VS
-		m_deviceContext->VSSetShader(m_vertexShader.Get(), nullptr, 0);
-
-		// RS
-
-		// PS
-		m_deviceContext->PSSetShader(m_pixelShader.Get(), nullptr, 0);
-
-		// OM
-		m_deviceContext->Draw(m_vertices.size(), 0);
-	}
 
 	RenderEnd();
 }
